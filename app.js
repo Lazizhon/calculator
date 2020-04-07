bdaa8c9be467cc7a58810bc8b76d1f6183242088
lazizhon <a.lazizhon@gmail.com>
2020-04-07 23:26:46 +0500

    V1 release

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let minus_btn =      document.getElementById('minus_btn');
 let equal_btn =      document.getElementById('equal_btn');
 let plus_btn =       document.getElementById('plus_btn');
 let decimal_btn =    document.getElementById('decimal_btn');
+let back_btn =       document.getElementById('back_btn');
 let answer_display = document.getElementById('answer_display');
 let zero_btn =       document.getElementById('zero_btn');
 let one_btn =   document.getElementById('one_btn');
@@ -19,20 +20,35 @@ let nine_btn =  document.getElementById('nine_btn');
 let number_button =     document.querySelectorAll('.number_button');
 let operator_button =   document.querySelectorAll('.operator_button');
 let button =            document.querySelectorAll('button');
+let inputArr = [];
 
 let chosenOperator = "";
+
+//Main Operators
 function addNumbers(one, two){
     answer_display.value =  one + two
     inputArr.length = 0;
     inputArr.push(answer_display.value);
 }
-
-function operate(){
-
+function minusNumbers(one, two){
+    answer_display.value =  one - two
+    inputArr.length = 0;
+    inputArr.push(answer_display.value);
+}
+function multiplyNumbers(one, two){
+    answer_display.value =  one * two
+    inputArr.length = 0;
+    inputArr.push(answer_display.value);
+}
+function divideNumbers(one, two){
+    answer_display.value =  one / two
+    inputArr.length = 0;
+    inputArr.push(answer_display.value);
 }
 
-let inputArr = [];
 
+
+// Number Entry
 number_button.forEach(function(el){ 
     el.addEventListener('click', function(){
         answer_display.value += el.innerHTML
@@ -45,26 +61,49 @@ decimal_btn.addEventListener('click', function(){
         answer_display.value += this.innerHTML
     }
 })
+// Operator Selection 
 operator_button.forEach(function(el){ 
     el.addEventListener('click', function(){
         // empty.push(el.innerHTML);
-        inputArr.push(answer_display.value);
+        if(answer_display.value != inputArr[0]){
+            inputArr.push(answer_display.value);
+        }
         answer_display.value = "";
         
         chosenOperator = el.innerHTML;
         console.log(chosenOperator);
+        console.log(inputArr);
         
     })
 })
 clear_btn.addEventListener('click', function(){
     answer_display.value = "";
     inputArr.length = 0;
+    console.log(inputArr);
+})
+back_btn.addEventListener('click', function(){
+    let deleteLastOne = answer_display.value
+    let assignIt = deleteLastOne.slice(0, -1)
+    answer_display.value = assignIt
+    // console.log(an);
 })
 equal_btn.addEventListener('click', function(){
-    inputArr.push(answer_display.value);
+    if(answer_display.value != ''){
+        inputArr.push(answer_display.value);
+    }
     if(chosenOperator == "+"){
         addNumbers(parseInt(inputArr[0]), parseInt(inputArr[1]))
     }
+    if(chosenOperator == "-"){
+        minusNumbers(parseInt(inputArr[0]), parseInt(inputArr[1]))
+    }
+    if(chosenOperator == "*"){
+        multiplyNumbers(parseInt(inputArr[0]), parseInt(inputArr[1]))
+    }
+    if(chosenOperator == "/"){
+        divideNumbers(parseInt(inputArr[0]), parseInt(inputArr[1]))
+    }
+
     console.log(inputArr);
 })
 
